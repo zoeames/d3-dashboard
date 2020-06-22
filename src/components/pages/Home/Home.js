0cad7ead6462eff15e004118bb536866169f2f33
Zoe Ames <zoeames@gmail.com>
2020-06-22 00:01:55 -0500

    setup complete

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,20 +1,30 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+
+import authData from '../../../helpers/data/auth';
 
 import './Home.scss';
 
 class Home extends React.Component {
-  editEvent = (e) => {
-    e.preventDefault();
-    const scatId = 'monkeybutt67';
-    this.props.history.push(`/edit/${scatId}`);
+  githubLogin = () => {
+    authData.getGithubAuth()
+      .then((resp) => console.log('github response: ', resp))
+      .catch((err) => console.error('unable to auth with github: ', err));
+  }
+
+  battleNetLogin = () => {
+    authData.getBattleNetAuth()
+      .then((resp) => console.log('battle net response: ', resp))
+      .catch((err) => console.error('unable to auth with github: ', err));
   }
 
   render() {
     return (
       <div className="Home col-12">
         <h1>Home</h1>
+        <button className="btn btn-dark" onClick={this.githubLogin}><i className="fab fa-github fa-3x"></i></button>
+        <button className="btn btn-dark" onClick={this.battleNetLogin}><i className="fad fa-swords fa-3x"></i></button>
       </div>
     );
   }
