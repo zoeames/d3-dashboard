2046ceccc8d371f419ebdd16eab7d6462b8d83e9
Zoe Ames <zoeames@gmail.com>
2020-06-25 08:52:45 -0500

    added call to profile - api down

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 // import { Link } from 'react-router-dom';
 import './Home.scss';
 import authData from '../../../helpers/data/authData';
+import userInfoData from '../../../helpers/data/userInfoData';
 
 class Home extends React.Component {
   state = {
@@ -10,22 +11,32 @@ class Home extends React.Component {
     battleTag: '',
   }
 
+  getProfile = (accessToken, battleTag) => {
+    userInfoData.getProfile(accessToken, battleTag)
+      .then((profile) => console.error(profile.data))
+      .catch((err) => console.error('unable to find your profile: ', err));
+  };
+
   componentDidMount() {
     authData.getBattleNetToken()
       .then((resp) => {
         localStorage.setItem('bnetToken', JSON.stringify(resp.data));
         this.setState({ bnetToken: resp.data });
         authData.getBattleTag(resp.data.access_token).then((resp2) => {
-          localStorage.setItem('battleTag', JSON.stringify(resp2.data.battletag));
-          this.setState({ battleTag: resp2.data.battletag });
+          const battleTag = resp2.data.battletag;
+          localStorage.setItem('battleTag', JSON.stringify(battleTag));
+          this.setState({ battleTag });
+          this.getProfile(resp.data.access_token, battleTag);
         });
       }).catch((err) => console.error('unable to get battle net token: ', err));
   }
 
   render() {
+    const { battleTag } = this.state;
     return (
       <div className="Home col-12">
         <h1>Home</h1>
+        <h2>Welcome {battleTag}</h2>
       </div>
     );
   }
